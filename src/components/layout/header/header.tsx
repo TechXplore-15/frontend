68cb24d828d508f0482d944bb14f04a00f753ce6
richi1213 <datonanava11@gmail.com>
2025-02-22 14:05:10 +0400

    added home nav

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -6,7 +6,7 @@ import {
   Tooltip,
 } from '@/components/ui/tooltip';
 import { PATHS } from '@/routes/enums';
-import { UserCheck } from 'lucide-react';
+import { House, UserCheck } from 'lucide-react';
 import React from 'react';
 import { Link } from 'react-router';
 
@@ -15,6 +15,21 @@ export const Header: React.FC = () => {
     <header className='text-sm bg-primary'>
       <nav className='sticky z-[100] h-10 w-full text-sm transition-all md:pl-2 mx-auto flex items-center justify-center'>
         <ul className='flex items-center justify-center gap-5'>
+          <li>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link to={PATHS.HOME} className='flex items-center'>
+                    <House className='size-[1.2rem] text-white hover:text-gray-900' />
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>მთავარი</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </li>
+
           <li>
             <TooltipProvider>
               <Tooltip>
